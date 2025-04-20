ee24b11d25ad77b0c88247d42d7c59a14330ff68
Fares426 <farishamza426@gmail.com>
2025-04-20 16:49:06 +0200

    Third commit

---
+++ src/ASW/src/asw/ASW.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asw;
+
+/**
+ *
+ * @author Softlaptop
+ */
+public class ASW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame lf = new NewJFrame();
+        lf.setLocation(100, 100);
+        lf.setVisible(true);
+        lf.setResizable(false);
+    }
+    
+}
